9426b33b89ee99a7802f66d7a21f86a54dd398bd
abhinn-mishra <117530009+abhinn-mishra@users.noreply.github.com>
2022-11-18 15:15:17 +0000

    Added Delete Link list

--- SingleLinkList.c
+++ SingleLinkList.c
@@ -28,6 +28,11 @@ void display() {
     struct Node *tmp  = NULL;
     tmp = head;
 
+    if(head == NULL) {
+        printf("\n List is EMPTY");
+        return;
+    }
+
     while(tmp != NULL) {
         printf("\n Node value is %d",tmp->data);
         tmp = tmp->next;
@@ -49,8 +54,73 @@ void add(int data) {
     }
 }
 
+void delete(int data) {
+    struct Node *tmp  = NULL;
+    struct Node *prev  = NULL;
+    tmp = head;
+
+    while(tmp != NULL) {
+
+      // data to be deleted found in the linked list
+    if(data == tmp->data) {
+    
+        // Logic to delete first/HEAD node
+        if(tmp == head) {
+            printf("\n Deleting HEAD node %d",tmp->data);
+            head = head->next;
+            tmp->next = NULL;
+            break;
+        }
+
+         // Logic to delete last/TAIL node
+        else if(tmp->next == NULL) {
+            printf("\n Deleting LAST node %d",tmp->data);
+            prev->next = NULL;
+            break;
+        } 
+
+        // Logic to delete middle nodes
+        else {
+            printf("\n Deleting Middle node %d",tmp->data);
+            prev->next = tmp->next;
+            tmp->next = NULL;
+            break;
+        }
+
+    } 
+        
+        prev = tmp;
+        tmp = tmp->next;
+    }
+
+}
+
 void main() {
+
+// printf("Size of struct Node --> %ld",sizeof(struct Node));
+// printf("Size of Integer --> %ld",sizeof(int));
 add(1);
+add(2);
+add(3);
+add(4);
+add(5);
+
 display();
+
+delete(1);
+display();
+
+delete(5);
+display();
+
+delete(3);
+display();
+
+delete(2);
+display();
+
+delete(4);
+display();
+
 }
 
