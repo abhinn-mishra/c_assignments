00c3e406600c4ef49a8e7cfabff4d4961977ac25
abhinn-mishra <117530009+abhinn-mishra@users.noreply.github.com>
2022-12-03 15:10:16 +0000

    Added Inorder Preorder Postorder Traversal

--- Tree.c
+++ Tree.c
@@ -29,6 +29,34 @@ struct Node* addNodeToLeft(struct Node *node,int data) {
     return leftNode;
 }
 
+void inOrder(struct Node *node) {
+    if(node == NULL) {
+        return;
+    }
+    inOrder(node->left);
+    printf(" %d ",node->data);
+    inOrder(node->right);
+}
+
+void preOrder(struct Node *node) {
+    if(node == NULL) {
+        return;
+    }
+
+    printf(" %d ",node->data);
+    preOrder(node->left);
+    preOrder(node->right);
+}
+
+void postOrder(struct Node *node) {
+    if(node == NULL) {
+        return;
+    }
+    postOrder(node->left);
+    postOrder(node->right);
+    printf(" %d ",node->data);
+}
+
 void main() {
     
     /*
@@ -48,11 +76,34 @@ void main() {
    struct Node *node2 = addNodeToLeft(root,2);
    struct Node *node3 = addNodeToRight(root,3);
    
-   struct Node *node4 = addNodeToRight(node2,4);
+   struct Node *node4 = addNodeToLeft(node2,4);
    
    printf("\nRoot Node %d",root->data);
    printf("\nLeft Node %d",node2->data);
    printf("\nRight Node %d",node3->data);
    printf("\nLeaf Node %d",node4->data);
 
+   printf("\n Inorder Traversal \n");
+   inOrder(root);
+
+   printf("\n preOrder Traversal \n");
+   preOrder(root);
+
+   printf("\n postOrder Traversal \n");
+   postOrder(root);
+
 }
+
+// In Order -- Left Root Right
+// 4 10 12 15 18 22 24 25 31 35 44 50 66 70 90
+
+//  Pre Order --  Root Left Right
+// 25 15 10 4 12 22 18 24 50 35 31 44 70 66 90
+
+
+// Post Order -- Left Right Root
+// 4 12 10 18 24 22 15 31 44 35 66 90 70 50 25
+
+
+// Post Order -- Left Right Root
+// 4 12 10 18 24 22 15 31 44 35 66 90 70 50 25
